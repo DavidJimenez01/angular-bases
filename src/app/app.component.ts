2ae6c812947e87f16da84348fbbe937b87fd2e63
David Jimenez <yjime13@bancodebogota.com.co>
2023-07-24 11:13:42 -0500

    primera version de la primera seccion del curso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] //llaves porque es un arreglo de estilos
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primera app de angular';
 }
